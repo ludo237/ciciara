5d3c001664a0acba49e0bbf51dcf3c4c16de8468
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-03-25 18:00:15 +0100

    Add socket io to vuejs ✨

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import VueSocketIO from "vue-socket.io";
+import SocketIO from "socket.io-client";
 import App from "@/App.vue";
 import "@/registerServiceWorker";
 import router from "@/router";
@@ -6,6 +8,17 @@ import store from "@/store";
 import "@/services/fonts";
 
 Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO("http://localhost:2370"),
+  vuex: {
+    store,
+    actionPrefix: "CSA_",
+    mutationPrefix: "CSM_"
+  }
+})
+);
 
 new Vue({
   router,
