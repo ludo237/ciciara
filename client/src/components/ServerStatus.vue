29299024f86d28799c11827cf8449bedd6ee81ad
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-03-25 19:30:02 +0100

    ✨ Add new realtime status component

---
+++ client/src/components/ServerStatus.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="inline-flex items-center">
+    <BaseIcon size="sm" icon="circle" class="mr-1" :class="statusColor" />
+    <div class="text-gray-700 capitalize">
+      <span class="mr-1">Server</span>
+      <span v-text="statusText" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import BaseIcon from "@/components/base/BaseIcon.vue";
+
+export default {
+  name: "ServerStatus",
+
+  components: {
+    BaseIcon
+  },
+
+  computed: {
+    statusText() {
+      if (this.isCiciaraOnline) {
+        return "online";
+      }
+
+      return "offline"
+    },
+
+    statusColor() {
+      if (this.isCiciaraOnline) {
+        return ["text-green-500"];
+      }
+      return ["text-gray-500"];
+    },
+
+    ...mapGetters(["isCiciaraOnline"]),
+  }
+}
+</script>
