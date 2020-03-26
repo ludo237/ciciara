b9506aab58f6c54a553da9284b1819c567826ed0
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-03-26 17:32:14 +0100

    ✨ Add new components to manage the chat section

---
+++ client/src/components/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="px-6 py-4 flex-1 overflow-y-scroll">
+    <!-- A message -->
+    <ChatMessage />
+  </div>
+</template>
+
+<script>
+import ChatMessage from "@/components/ChatMessage.vue";
+
+export default {
+  name: "Chat",
+
+  components: {
+    ChatMessage
+  }
+}
+</script>
---
+++ client/src/components/ChatMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-start mb-4 text-sm">
+    <div
+      class="w-10 h-10 rounded mr-3 flex items-center justify-center text-3xl text-gray-500"
+    >
+      <BaseIcon icon="user" />
+    </div>
+    <div class="flex-grow overflow-hidden">
+      <div class="flex justify-between">
+        <span class="font-semibold">{ username }</span>
+        <span class="text-gray-500 text-xs">11:46</span>
+      </div>
+      <p class="text-black leading-normal">
+        The slack from the other side.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseIcon from "@/components/base/BaseIcon.vue";
+
+export default {
+  name: "ChatMessage",
+
+  components: {
+    BaseIcon
+  }
+};
+</script>
