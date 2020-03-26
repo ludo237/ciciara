cdf7d3497278fa513bddd1a5910174c08ad4386d
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-03-26 17:32:42 +0100

    ✨ Add UserStatus component

---
+++ client/src/components/UserStatus.vue
@@ -0,0 +1,21 @@
+<template>
+  <BaseIcon size="xs" icon="circle" class="mr-1" :class="statusColor" />
+</template>
+
+<script>
+import BaseIcon from "@/components/base/BaseIcon.vue";
+
+export default {
+  name: "UserStatus",
+
+  components: {
+    BaseIcon
+  },
+
+  computed: {
+    statusColor() {
+      return ["text-green-500"];
+    }
+  }
+};
+</script>
