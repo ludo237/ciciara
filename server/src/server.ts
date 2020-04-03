efb92fc6acaf16d20d92f6c9302960f7f3a9ba74
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-04-03 17:16:57 +0200

    🐛 Resolve issue with socket using the new Api system

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Express from "express";
+import Socket from "socket.io";
 
 // Local Imports
 import App from "./app";
@@ -10,18 +11,17 @@ const devMode = process.env.NODE_ENV === "development";
 // Initialize Dotenv
 require("dotenv").config({ path: ".env.local" });
 
-// Initialize Socket
-const io = require("socket.io")(require("http").Server(App));
-
 // Initialize ORM
 const db = process.env.DB_URL || "";
 connect({ db });
 
-io.on("connection", async (socket: any) => {
-  console.log("Someone connected using socket");
+// Start the server
+const server = App.listen(process.env.API_PORT, () => {
+  console.log(`Server is up and running on port: ${process.env.API_PORT}`);
 });
 
-// Start the server
-App.listen(process.env.API_PORT, () => {
-  console.log(`Server is up and running on port: ${process.env.API_PORT}`);
+let io = Socket(server);
+
+io.on("connection", async (socket: any) => {
+  console.log("Someone connected using socket");
 });
